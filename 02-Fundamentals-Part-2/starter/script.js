e6e8a233406960b319e9774d57df54c8a212303e
sindrifj@gmail.com <sindrifj@gmail.com>
2023-05-03 16:59:58 +0000

    ff

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -49,3 +49,78 @@ const yearsUntilRetirement = (birthYeah, firstName) => {
   }
   
   console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+  const friends = ["Sindri","Einar "]
+
+  friends.length // lengd
+  friends[0] // stak 0
+  friends.push("maggi") // bæta við afstast listann skilar lengd listans
+  friends.unshift("Palli") // bætir við fremst í listann
+  friends.pop() // eyðir aftasta stakinu skilar eyddu staki
+  friends.shift() // eyðir fremsta stakinu
+  friends.indexOf("Sindri") // sýnir númerið á staki
+  friends.includes("Sindri") // skilar true ef það er til staðar í lysta
+
+const notandi = {
+    forNafn: "Sindir",
+    eftirNafn: "Fanndal"
+}
+
+console.log(notandi.forNafn)
+console.log(notandi["eftirNafn"]) // nota þetta frekar til að taka á móti streng og leita af honum.
+
+// bæta við 
+notandi.stadur = "Garður";
+notandi["heimilisfang"] = "birkitún"
+console.log(notandi)
+
+// hægt er að nota function í object svona 
+const notandi2 = {
+    forNafn: "Sindir",
+    eftirNafn: "Fanndal",
+    faedingarAr: 1986,
+    calcAge3: birthYeah => 2037 - birthYeah,
+    calcAge4: function () {
+        return 2037 - this.faedingarAr; // this er objectið semagt notandi2
+    },
+    
+    calcAge5: function () {
+        this.aldur = 2037 - this.faedingarAr;
+        return this.aldur;
+    }
+};
+
+console.log(notandi2.calcAge3(2000));
+
+// vitna í object í function sem dæmi erum við að nota faedingarar til að reikna út aldur
+console.log(notandi2.calcAge4());
+
+// best er samt að reikna valuið þegar objectið er búið til og sækja það svo þegar maður þarf
+console.log(notandi2.calcAge5());
+console.log(notandi2.aldur);
+
+
+// búum til variable fyrst síðan ef variable er undir eða sama og 10 þá bætum við einum við 
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`wirk ${rep}`)
+};
+
+// rúlla í gegnum lista
+const listi = ["sidnri","Kalli","Einar"];
+for (let i = listi.length -1; i >= 0; i-- ){
+    console.log(listi[i]);
+}
+
+for (let exer = 1; exer < 4; exer++){
+    console.log(`starting exer ${exer}`);
+    for (let rep = 1;rep < 6; rep++){
+        console.log(`reps ${rep}`)
+    }
+}
+// notum while ef við þurfum ekki counter
+let rep = 1
+while (rep <= 10){
+    console.log(`WHILE reps ${rep}`);
+    rep++;
+}
---
+++ 02-Fundamentals-Part-2/starter/test.py
@@ -0,0 +1,7 @@
+rep = 8
+for rep in range(10):
+    print(rep)
+
+for x in rep:
+    if x <= 10:
+        rep = rep+1
